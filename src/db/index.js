a60b1856e3133293f553d2c9b6f6fd9cc1d12b9f
sawan patil <sawanpatil1711@gmail.com>
2026-06-06 17:03:11 +0530

    feat: add database connection and environment configuration
    - Added dotenv for environment variable management.
    - Created a database connection module using mongoose.
    - Introduced constants for database name.
    - Updated package.json to include prettier and necessary dependencies.
    - Added .prettierrc and .prettierignore files for code formatting.

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import {DB_NAME} from '../constants.js';
+import mongoose from 'mongoose';
+
+const connectDB = async ()=>{
+    try{
+        const connectionInstance =await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    }catch(error){
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
