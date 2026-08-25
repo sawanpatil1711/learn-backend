e9009f96cbaf44c22dd715b87e9d34ebdc8c9aa4
sawan patil <sawanpatil1711@gmail.com>
2026-08-25 18:36:25 +0530

    fix: correct import case for ApiError and ApiResponse in tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/apiError.js";
-import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { Tweet } from "../models/tweet.model.js";
 import mongoose from "mongoose";
 
