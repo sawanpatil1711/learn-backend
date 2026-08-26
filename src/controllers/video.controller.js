e08e4e8532f5a81bc3a75b1ccbd1d0b15fd836f1
sawan patil <sawanpatil1711@gmail.com>
2026-08-26 14:33:58 +0530

    fix: correct import case for ApiError and ApiResponse in tweet controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,9 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
-import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const uploadVideo = asyncHandler(async (req, res)=>{
     const {title, description} = req.body
